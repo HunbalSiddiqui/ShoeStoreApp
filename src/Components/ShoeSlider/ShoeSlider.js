90b91d64bb92e86551780c9940c85e246a118260
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-09-04 02:58:35 +0500

    Slider integrated

--- src/Components/ShoeSlider/ShoeSlider.js
+++ src/Components/ShoeSlider/ShoeSlider.js
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import './ShoeSlider.css'
+import one from './homepackage/one.jpg'
+import two from './homepackage/two.jpg'
+import three from './homepackage/three.jpg'
+import four from './homepackage/four.jpg'
+import five from './homepackage/five.jpg'
+import six from './homepackage/six.jpg'
+import seven from './homepackage/seven.jpg'
 export default function ShoeSlider() {
     return (
-        <div>
+        <div className="flex">
+            <div className="image_card">
+            </div>
+            <div className="image_card">
+                <img src={one} style={{width:'100%',height:'100%'}}></img>
+            </div>
+            <div className="image_card">
+                <img src={two} style={{width:'100%',height:'100%'}}></img>
+            </div>
+            <div className="image_card">
+                <img src={three} style={{width:'100%',height:'100%'}}></img>
+            </div>
+            <div className="image_card">
+                <img src={four} style={{width:'100%',height:'100%'}}></img>
+            </div>
+            <div className="image_card">
+                <img src={five} style={{width:'100%',height:'100%'}}></img>
+            </div>
+            <div className="image_card">
+                <img src={six} style={{width:'100%',height:'100%'}}></img>
+            </div>
+            <div className="image_card">
+                <img src={seven} style={{width:'100%',height:'100%'}}></img>
+            </div>
             
         </div>
     )
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,16 @@
 import React,{Fragment} from 'react'
 import './Home.css'
+import ShoeSlider from '../../Components/ShoeSlider/ShoeSlider'
 export default function Home() {
     return (
     <Fragment>    
         <div className="home_container">
             <div className="home_wrapper">
             </div>
+            <div className="slide_wrapper flex" id="slider">
+                <ShoeSlider/>
+            </div>
         </div>
-        
     </Fragment>
     )
 }
